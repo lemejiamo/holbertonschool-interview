9a11225e26c37e68d10acfa333fb4a1fb633f1c5
debian <debian@debian>
2021-12-16 18:48:05 +0100

    insert in linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -74,3 +74,35 @@ void free_listint(listint_t *head)
 }
 
 
+/**
+ * insert_node - insert a node in a sorted list
+ * @head: pointer to the head of the list
+ * @number: number to insert
+ * Return: pointer to a new node or null if fail
+ */
+listint_t  *insert_node(listint_t **head, int number)
+{
+	listint_t *copy_head = NULL;
+	listint_t *new_node = NULL;
+
+	copy_head = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return NULL;
+
+	new_node->n = number;
+
+	while(new_node->n >= copy_head->n)
+	{
+		if (copy_head->next->n < new_node->n)
+			copy_head = copy_head->next;
+		else
+			break;
+	}
+
+	new_node->next = copy_head->next;
+	copy_head->next = new_node;
+	return new_node;
+
+}
