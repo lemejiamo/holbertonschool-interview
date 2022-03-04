14c967151bc28fd6383880f617075857a236f1e1
lemejiamo <77980644+lemejiamo@users.noreply.github.com>
2022-03-03 19:07:11 -0500

    Add files via upload

---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,11 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void menger(int level);
+char find_space(int col, int row);
+
+#endif /* MENGER_H */
