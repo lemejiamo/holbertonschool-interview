6845a2b1ea85835a1d07912507969bb6aa621251
Luis Enrique Mejia M <77980644+lemejiamo@users.noreply.github.com>
2022-08-05 03:14:35 +0200

    Add files via upload

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+""" Method to open locked boxes"""
+
+
+def canUnlockAll(boxes):
+    """ method to chexk if all boxes can be unlocked"""
+
+    def find_keys(box):
+        """return the keys in the box"""
+        box_keys = []
+        for key in box:
+            box_keys.append(key)
+        return box_keys
+
+    def box_quantity(boxes):
+        """return the number of boxes to check"""
+        return (len(boxes))
+
+    def init_state(box_quantity):
+        """init the status list"""
+        box_state = {}
+        box_quantity -= 1
+        while (box_quantity > 0):
+            box_state[box_quantity] = ['close', 'uncheck']
+            box_quantity -= 1
+        box_state[0] = ['open', 'uncheck']
+        return (box_state)
+
+    def open_box(box_state, box_number, box_quantity):
+        """open a box"""
+        if box_state[box_number] != 'open':
+            box_state[box_number] = 'open'
+        return (box_state)
+
+    def check_box(keys, b_state, quantity):
+        """funtion to check the boxes"""
+
+        for key in keys:
+            if key < box_quantity and key >= 0:
+                if b_state[key][0] == 'open' and b_state[key][1] == 'uncheck':
+
+                    box_keys = find_keys(boxes[key])
+
+                    b_state[key][1] = 'check'
+
+                    for key in box_keys:
+                        if key < box_quantity and key >= 0:
+                            b_state[key][0] = 'open'
+                        else:
+                            pass
+                        b_state = check_box(box_keys, b_state, quantity)
+                else:
+                    pass
+            else:
+                pass
+
+        return (b_state)
+    # |----------------- end of methods --------------------|
+
+    box_quantity = box_quantity(boxes)
+
+    box_state = init_state(box_quantity)
+    box_state = check_box([0], box_state, box_quantity)
+
+    for key, value in box_state.items():
+        if value[0] == "close":
+            return False
+    return True
