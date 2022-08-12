2966e91318ec290d41ef0779c4b15075032e5285
Luis Enrique Mejia M <77980644+lemejiamo@users.noreply.github.com>
2022-08-12 05:59:19 +0200

    Add files via upload

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+minimum operations to print n Characters
+"""
+
+
+def minOperations(n):
+    """
+    Calculates the minimum number of operations to reply 'n' number  of chars
+    """
+    # If n is 1, 0 operations are needed.
+    if n <= 1:
+        return 0
+
+    # Find smallest prime factors
+    for i in range(2, int((n/2)+1)):
+        if n % i == 0:
+            return minOperations(int(n / i)) + i
+
+    return n
