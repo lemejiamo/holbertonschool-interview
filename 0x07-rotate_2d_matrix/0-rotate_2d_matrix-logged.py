b0efbd9d39adcf206a1eeaa303276d100cbd8a82
LEMM <luismejia69@gmail.com>
2022-09-15 22:01:20 -0500

    commited version

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix-logged.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix-logged.py
@@ -18,24 +18,41 @@ def rotate_2d_matrix(matrix):
 
     for _row in range(matrixLength - 1):
         print(f'row {_row}')
+        print(f'jump is {jump}')
+        _column = _row
 
-        for _column in range(jump):
+        while _column < (jump + _row):
+            
+        #for _column in range(jump):
             print(f'the column is {_column}')
             print(f'el salto es {jump}')
             # set origin
-            if _column < _row:
-                origin = [_row, _row]
-            else:
-                origin = [_row, _column]
+            # if _column < _row:
+            #     origin = [_row, _row]
+            # else:
+            #     origin = [_row, _column]
+
+            origin = [_row, _column]
+
             print(f'the origin is {origin}')
             current = []
 
-            if jump == 1 or jump == 2:
-                rowFactor, columnFactor = 0, jump
-            else:
-                rowFactor = origin[1]
-                columnFactor = jump - origin[1]
+            # if jump == 1:
+            #     rowFactor, columnFactor = 0, jump
+            # elif jump == 2:
+            #     if origin[1] - origin[0] == 1:
+            #         rowFactor, columnFactor = 1, 1
+            #     elif origin[1] - origin[0] == 0:
+            #         rowFactor, columnFactor = 0, 2
+            #     else:
+            #         rowFactor, columnFactor = 2, 0
+            # else:
+            #     rowFactor = origin[1]
+            #     columnFactor = jump - origin[1]
 
+            rowFactor = origin[1] - origin[0]
+            columnFactor = jump - rowFactor
+            
                 # parametros por defecto
             temp: int = None
             counter = 0
@@ -88,7 +105,10 @@ def rotate_2d_matrix(matrix):
                 print()
                 print()
                 print()
+            _column = _column + 1
 
+            for line in matrix:
+                print(line)
         jump = jump - 2
         if jump < 1:
             break
